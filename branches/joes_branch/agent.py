bf6dbf6644b2dcbe4ef54b90d3ae746335869515
joe@mcc4ll.us <joe@mcc4ll.us@4acb46ae-e8f5-11dd-85a8-df16bb8a7048>
2009-03-01 02:15:14 +0000

    Creating a new branch for me (Joe) to work on assignment 2.

---
+++ branches/joes_branch/agent.py
@@ -0,0 +1,45 @@
+from direct.actor.Actor import Actor
+from direct.task import Task
+import direct.directbase.DirectStart
+
+class Agent(Actor):
+    
+    def __init__(self, modelStanding, modelAnimationDict, turnRate, speed, agentList):
+        Actor.__init__(self, modelStanding, modelAnimationDict)
+        self.turnRate = turnRate
+        self.speed = speed
+        self.agentList = agentList
+        
+        if self not in agentList:
+            self.agentList.append(self)
+    
+    def turnLeft(self, angle):
+        self.setH(self.getH() + angle)
+        return
+    
+    def turnRight(self, angle):
+        self.setH(self.getH() - angle)
+        return
+    
+    def moveForward(self, distance):
+        # Gets the net transform from render as a matrix, and get the second
+        # row, which represents the transform on the y-axis (in front of us)
+        backward = self.getNetTransform().getMat().getRow3(1)
+        
+        # Normalize the vector
+        backward.normalize()
+        
+        # Now move our  forward
+        self.setPos(self.getPos() - backward * distance)
+        return
+    
+    def moveBackward(self, distance):
+        backward = self.getNetTransform().getMat().getRow3(1)
+        backward.normalize()
+        self.setPos(self.getPos() + backward * distance)
+        return
+
+
+if __name__ == "__main__":
+    A = Agent("models/ralph", {"run":"models/ralph-run"}, turnRate = 300, speed = 5)
+    print(" compiled correctly")
