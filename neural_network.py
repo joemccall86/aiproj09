6c7340b913048ed45395a1b9d4382737047b558a
joe@mcc4ll.us <joe@mcc4ll.us@4acb46ae-e8f5-11dd-85a8-df16bb8a7048>
2009-03-20 00:33:13 +0000

    refactored some code in world.py

--- neural_network.py
+++ neural_network.py
@@ -41,8 +41,6 @@ class NeuralNetwork():
                 distance = math.hypot(x2-x1, y2-y1)
                 # Since we want to minimize distance, just make the fitness the negative of the distance
                 chromo.fitness = 500 - distance
-                
-                print "chromo" , i , chromo.fitness
         
         # Point the fitness function to the one we just defined
         population.Population.evaluate = fitness
